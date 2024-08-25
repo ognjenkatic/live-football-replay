9d6d435060d411224f967df69b4e61bb7f048f7e
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2024-08-25 16:53:10 +0200

    Move project to appropriate dir

---
+++ src/LiveFootball.Application/Matches/Models/MatchSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace LiveFootball.Application.Matches.Models
+{
+    public class MatchSummary
+    {
+        public required int MatchId { get; set; }
+        public required string HomeTeam { get; set; }
+        public required string AwayTeam { get; set; }
+        public required int HomeGoals { get; set; }
+        public required int AwayGoals { get; set; }
+    }
+}
